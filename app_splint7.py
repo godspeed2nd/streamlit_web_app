4ec9f96f1e7ac0b4e3be808dcfea719464cb5021
gods <godspeed.2nd@gmail.com>
2024-10-18 00:07:06 +0900

    9.2 th test

--- app_splint7.py
+++ app_splint7.py
@@ -173,7 +173,7 @@ with st.form(key='profile_form'):
 #                img_path = os.path.join(IMG_PATH, file.name)
                 img_path = os.path.join(uploaddir, file.name)
                 # test
-                img_path = 'C:\temp\pythonProject1\streamlit\imgs\IMG_2207.PNG'
+                img_path = r'C:\temp\pythonProject1\streamlit\imgs\IMG_2207.PNG'
                 # test
                 print("img_path") 
                 print(img_path)
